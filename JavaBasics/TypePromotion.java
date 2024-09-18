d68197b6fe9c1e97d7c675e934b63dce1da85299
Anurag0306 <karamveer0306@gmail.com>
2024-09-18 17:45:51 +0530

    TypePromotion

---
+++ JavaBasics/TypePromotion.java
@@ -0,0 +1,18 @@
+public class TypePromotion {
+    public static void main (String args[]){
+
+        // char a = 'a';
+        // char b = 'b';
+
+        // System.out.println(b-a);
+
+        int a =10;
+        float b =22.5f;
+        long c = 12;
+        double d = 55;
+        double ans = a+b+c+d;
+
+        System.out.println(ans);
+    }
+    
+}
