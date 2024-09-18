18039f916fa833a6f61de8c45026a9dd8bb24561
Anurag0306 <karamveer0306@gmail.com>
2024-09-18 15:14:28 +0530

    Variable

---
+++ JavaBasics/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main (String args[]){
+        int a = 10;
+        int b = 5;
+        String name = "Anurag";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+
+
+    }
+    
+}
