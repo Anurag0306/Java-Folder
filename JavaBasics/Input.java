3b8ea1b5b94e77e65c1bc3413fbbd6022e017fe8
Anurag0306 <karamveer0306@gmail.com>
2024-09-18 16:47:49 +0530

    JavaBasics

---
+++ JavaBasics/Input.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Input {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in );
+
+        // WE USE next TO TAKE INPUT OF  A SINGLE WORD //                        
+
+        /* String input = sc.next();
+        System.out.println(input);
+        */
+
+        //  WE USE nextLine TO TAKE INPUT OF WHOLE PARA OR SENTENCE //  
+
+        /* String name = sc.nextLine();
+        System.out.println(name);
+        */
+
+         //  WE USE nextInt TO TAKE INPUT OF A NUMBER //  
+
+        /* int number = sc.nextInt();
+        System.out.println(number);
+        */
+
+        //  WE USE nextInt TO TAKE INPUT OF A DECIMAL NUMBERS //  AND SO ON //
+
+        Float price = sc.nextFloat();
+        System.out.println(price);
+
+
+
+    }
+    
+}
+
+// INPUT IN JAVA
+//   -next
+//   -nextLine
+//   -nextInt
+//   -nextFloat
+//   -nextByte
+//   -nextDouble
+//   -nextBoolean
+//   -nextShort
+//   -nextLong
